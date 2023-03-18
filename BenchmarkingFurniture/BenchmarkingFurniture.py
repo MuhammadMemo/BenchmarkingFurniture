18e4cd184deaaeebf811c99429d186a8a7c78838
Muhammad Mahmoud <programmer.muhammad@gmail.com>
2023-03-18 19:42:52 +0200

    v5

--- BenchmarkingFurniture/BenchmarkingFurniture.py
+++ BenchmarkingFurniture/BenchmarkingFurniture.py
@@ -20,7 +20,7 @@ List_Company=['0..all','1..Mffco','2..Kabbani','3..Egypt','4..Hub'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
-df = pd.read_excel("Datafurniture.xls")
+#df = pd.read_excel("Datafurniture.xls")
 #url="https://github.com/MuhammadMemo/BenchmarkingFurniture/blob/6e044ecf25f743ee036dd804d3afc96e0f48c673/BenchmarkingFurniture/Datafurniture.xls"
 
 #file=requests.get(url)
@@ -92,57 +92,55 @@ def ElMalik(url, headers, campany, category):
     AllData.clear()
  
     return DFElMalik
-    #Save Data In excel sheet
 
-    #Mathod Get Mffco data
+    # Mathod Get Mffco data
 def Mffco(url, headers, campany, category):
+    # Get Campany ,Category name
+    campanyName = campany
+    categoryName = category
+    url=url
+    # Get Page HTML
+    page = rs.get(url=url, headers=headers)
+    soup = bs(page.content, 'html.parser')
 
-    for g in range(len(url)):
-        #Get Campany ,Category name
-        campanyName = campany[g]
-        categoryName = category[g]
-        #Get Page HTML
-        page = rs.get(url=url[g], headers=headers)
-        soup = bs(page.content, 'html.parser')
-
-        #Filter Products in HTML
-        filter_Products = soup.find_all("div", class_='product_container')
-        #Loop Get Product name
-        for i in filter_Products:
-            for p in i.find_all("h3", class_='title'):
-                Products.append(p.text.strip())
-                 #Get category,campany name
-                CampanyList.append(campanyName)
-                CategoryList.append(categoryName)
-            #Loop Get Price
-            global FlagPrice
-            for c in i.find_all(class_='woocommerce-Price-amount amount'):
-                if (FlagPrice == 0):
-                     PriceBeforDiscount.append(c.text)
-                     FlagPrice=1
-                else:
-                    Price.append(c.text)
-                    FlagPrice=0
-            #Loop Get Images name
-            #for g in i.find_all('img'):
-            #    Img.append(g['src'])
-        # Sleep before Next URL
-        t.sleep(10)
+    # Filter Products in HTML
+    filter_Products = soup.find_all("div", class_='product_container')
+    # Loop Get Product name
+    for i in filter_Products:
+        for p in i.find_all("h3", class_='title'):
+            Products.append(p.text.strip())
+                # Get category,campany name
+            CampanyList.append(campanyName)
+            CategoryList.append(categoryName)
+        # Loop Get Price
+        global FlagPrice
+        for c in i.find_all(class_='woocommerce-Price-amount amount'):
+            if (FlagPrice == 0):
+                    PriceBeforDiscount.append(c.text)
+                    FlagPrice=1
+            else:
+                Price.append(c.text)
+                FlagPrice=0
+        #Loop Get Images name
+        #for g in i.find_all('img'):
+        #    Img.append(g['src'])
+    # Sleep before Next URL
+    t.sleep(10)
 # Image Download
-          #  
-       # t.sleep(100)
-    #    for u in range(len(img)):
-    #       opener = urlReq.build_opener()
-    #       opener.addheaders = [('User-Agent', 'MyApp/1.0')]
-    #       urlReq.install_opener(opener)
-    #       imgUrl="https:" + img[u]
-    # #     urlReq.urlretriev(imgUrl,str(u)+".jpg"+ Campany +"/"+ Category + "/" +"Name")
-    #       imgList.append(imgUrl)
+        #  
+    # t.sleep(100)
+#    for u in range(len(img)):
+#       opener = urlReq.build_opener()
+#       opener.addheaders = [('User-Agent', 'MyApp/1.0')]
+#       urlReq.install_opener(opener)
+#       imgUrl="https:" + img[u]
+# #     urlReq.urlretriev(imgUrl,str(u)+".jpg"+ Campany +"/"+ Category + "/" +"Name")
+#       imgList.append(imgUrl)
         
-        AllData = {'Campany': CampanyList, 'Category': CategoryList,
-                   'Products': Products, 'Price': Price,'PriceBeforDiscount':PriceBeforDiscount}  
-    #global FinalDatadf
-        DFMffco = pd.DataFrame(AllData)
+    AllData = {'Campany': CampanyList, 'Category': CategoryList,
+                'Products': Products, 'Price': Price,'PriceBeforDiscount':PriceBeforDiscount}  
+#global FinalDatadf
+    DFMffco = pd.DataFrame(AllData)
   
     #clear All variables
     CampanyList.clear()
@@ -154,38 +152,42 @@ def Mffco(url, headers, campany, category):
     AllData.clear()
     return DFMffco
  
-def getElMalikData():
+#def getElMalikData():
       #CampanyElMalik = dfElMalik['Campany']
       #CategorElMalik = dfElMalik['Category']
       #urlElMalik = dfElMalik['URL']
       #kdf=ElMalik(urlElMalik, headers, CampanyElMalik, CategorElMalik)
-      FinalDatadf=FinalDatadf.append(df,ignore_index=True)
+      #FinalDatadf=FinalDatadf.append(df,ignore_index=True)
 
 def getMffcoData():
-    
-    dfMffco = df.loc[df['Campany'] == 'Mffco']
+   
+    df = pd.read_excel("Datafurniture.xls")
+    dfMffco = df[df['Campany'] == 'Mffco']
+    dfMffco.reset_index(inplace=True)
+
     CampanyMffco = dfMffco['Campany']
     CategoryMffco = dfMffco['Category']
     urlMffco = dfMffco['URL']
-    mdf=Mffco(urlMffco, headers, CampanyMffco, CategoryMffco) 
+    for g in range(len(urlMffco)):
+         mdf=Mffco(urlMffco[g], headers, CampanyMffco[g], CategoryMffco[g]) 
+
     FinalDatadf=FinalDatadf.append(mdf,ignore_index=True)
 
 
 def main():
-    #df["Subjects"].unique())
 
     #List_Company=['0..all','1..Mffco','2..Kabbani','3..Egypt','4..Hub'
     #        ,'5 Smart','6..Carpiture','7..American']
-
-    while True :
-       
-       for c in List_Company:
+    for c in List_Company:
            print(c)
 
+    while True :
        Campany = int(input('Please enter an Campany Number :\n'))
-       if Campany==1 : 
-          #getMffcoData()
-               print(1)
+       if Campany==0 : 
+            print(0)
+       elif Campany==1 : 
+          getMffcoData()
+          print(1)
        elif Campany==2:
             # getElMalikData()
               print(2)
@@ -209,18 +211,7 @@ def main():
         #Save Data In Excel
      #  FinalDatadf.to_excel("h:\Product_Details.xlsx")
         
-      #while True:
-     #   city, month, day = get_filters()
-     #   df = load_data(city, month, day)
-     #   time_stats(df)
-     #   station_stats(df)
-     #   trip_duration_stats(df)
-     #   user_stats(df)
-     #   # data_show(df)
-     #   data_display(df)
-     #   restart = input('\nWould you like to restart? Enter yes or press any key to exit.\n')
-     #   if restart.lower() != 'yes':
-     #       break
+  
     print('Done')
 
    
